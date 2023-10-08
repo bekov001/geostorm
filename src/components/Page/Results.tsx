f588022fd9883eb80c7445e865377d0e1a965bcb
John Doe <johndoe@email.com>
2023-10-08 15:54:56 +0600

    modified results

--- src/components/Page/Results.tsx
+++ src/components/Page/Results.tsx
@@ -2,6 +2,7 @@ import {
     Bar,
     CartesianGrid,
     ComposedChart,
+    ResponsiveContainer,
     Tooltip,
     XAxis,
     YAxis
@@ -9,66 +10,100 @@ import {
 
 const data = [
     {
-      name: "Oct 7",
+      name: "Oct 7, 0:00",
       uv: 2.33,
     },
     {
-      name: "Oct 8",
+      name: "Oct 7, 3:00",
+      uv: 2.33,
+    },
+    {
+      name: "Oct 7, 6:00",
+      uv: 2.33,
+    },
+    {
+      name: "Oct 7, 9:00",
+      uv: 2.33,
+    },
+    {
+      name: "Oct 7, 12:00",
+      uv: 2.33,
+    },
+    {
+      name: "Oct 7, 15:00",
+      uv: 2.33,
+    },
+    {
+      name: "Oct 7, 18:00",
+      uv: 2.33,
+    },
+    {
+      name: "Oct 7, 21:00",
+      uv: 2.33,
+    },
+    {
+      name: "Oct 8, 0:00",
       uv: 2,
     },
     {
-      name: "Oct 9",
-      uv: 1,
+      name: "Oct 8, 3:00",
+      uv: 2.33,
     },
     {
-      name: "Oct 10",
-      uv: 1.5,
+      name: "Oct 8, 6:00",
+      uv: 2.33,
     },
     {
-      name: "Oct 11",
-      uv: 1,
+      name: "Oct 8, 9:00",
+      uv: 2.33,
     },
     {
-      name: "Oct 12",
-      uv: 6,
+      name: "Oct 8, 12:00",
+      uv: 2.33,
     },
     {
-      name: "Oct 13",
-      uv: 6,
+      name: "Oct 8, 15:00",
+      uv: 2.33,
     },
     {
-      name: "Oct 14",
-      uv: 6,
+      name: "Oct 8, 18:00",
+      uv: 2.33,
+    },
+    {
+      name: "Oct 8, 21:00",
+      uv: 2.33,
     },
   ];
 
 function Results() {
   return (
-    <div style={{position: "absolute", marginTop: 50}}>
-        <h1 className="headerone">Results</h1>
-        <h3 className="headerthree">k-index</h3>
-        <ComposedChart
-            width={1300}
-            height={400}
-            data={data}
-            margin={{
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: 20
-            }}
-            >
-            <CartesianGrid stroke="#f5f5f5" />
-            <XAxis dataKey="name" scale="band"/>
-            <YAxis
-                label={{ value: 'Kp index', angle: -90, position: 'insideLeft' }}
-                domain={[0, 9]} // Modify the Y-axis range
-                ticks={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]} // Set custom ticks
-            />
-            <Tooltip />
-            <Bar dataKey="uv" barSize={40} fill="#413ea0" />
-        </ComposedChart>
-    </div>
+    <>
+        <h1 className="headerone">asdfajsdhlf</h1>
+        <h3 className="headerthree">asoibdfbasd</h3>
+        <ResponsiveContainer width="100%" height={400}>
+            <ComposedChart
+                width={2000}
+                height={400}
+                data={data}
+                margin={{
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 20
+                }}
+                >
+                <CartesianGrid stroke="#f5f5f5" />
+                <XAxis dataKey="name" scale="band"/>
+                <YAxis
+                    label={{ value: 'Kp index', angle: -90, position: 'insideLeft' }}
+                    domain={[0, 9]} // Modify the Y-axis range
+                    ticks={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]} // Set custom ticks
+                />
+                <Tooltip />
+                <Bar dataKey="uv" barSize={40} fill="#413ea0" />
+            </ComposedChart>
+        </ResponsiveContainer>
+    </>
   );
 }
 
